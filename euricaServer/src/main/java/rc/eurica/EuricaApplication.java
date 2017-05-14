52595c624eebb65b655778daa6da36008c1ab2df
bysano <bysano14@gmail.com>
2017-05-14 12:30:01 +0300

    eurica server

---
+++ euricaServer/src/main/java/rc/eurica/EuricaApplication.java
@@ -0,0 +1,19 @@
+package rc.eurica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by alka0317 on 5/3/2017.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EuricaApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EuricaApplication.class, args);
+    }
+
+}
