e19b72725b2983acb7781aec02a4054ae646d3ba
Aliaksandr Kazlouski <aliaksandr.kazlouski@netcracker.com>
2017-06-19 11:55:55 +0300

    swagger news service added

--- newsMongoserviece/src/main/java/by/news/controller/MongoController.java
+++ newsMongoserviece/src/main/java/by/news/controller/MongoController.java
@@ -5,21 +5,16 @@ import by.news.domain.News;
 import by.news.domain.User;
 import by.news.services.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 
 @RestController
+@RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class MongoController {
 
     private RestTemplate restTemplate;
